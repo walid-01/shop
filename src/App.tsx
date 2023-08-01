65519dcb8fdceb52ecab6b89619d28559270f4eb
walid-01 <71653653+walid-01@users.noreply.github.com>
2023-08-01 18:47:56 +0100

    bit of navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import Navbar from "./components/Navbar";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import Store from "./pages/Store";
 
 function App() {
-  return <div className=""></div>;
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/store" element={<Store />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,79 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  const [page, setPage] = useState(0);
+
+  return (
+    <div className="shadow-md h-16 mb-3 w-full flex place-content-between items-center px-6 bg-white">
+      <ul className="flex content-center items-cente place-content-between">
+        <li
+          className={page === 0 ? "mr-6 font-medium" : "mr-6"}
+          onClick={() => setPage(0)}
+        >
+          <Link to="/">Home</Link>
+        </li>
+        <li
+          className={page === 1 ? "mr-6 font-medium" : "mr-6"}
+          onClick={() => setPage(1)}
+        >
+          <Link to="/about">About</Link>
+        </li>
+        <li
+          className={page === 2 ? "mr-6 font-medium" : "mr-6"}
+          onClick={() => setPage(2)}
+        >
+          <Link to="/store">Store</Link>
+        </li>
+      </ul>
+      <button className="w-12 h-12 border-2 border-gray-300">
+        <svg
+          fill="#000000"
+          viewBox="0 0 24 24"
+          id="cart"
+          data-name="Flat Line"
+          xmlns="http://www.w3.org/2000/svg"
+          className="icon flat-line"
+        >
+          <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+          <g
+            id="SVGRepo_tracerCarrier"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          ></g>
+          <g id="SVGRepo_iconCarrier">
+            <polygon
+              id="secondary"
+              points="21 7 19 15 8 16 6.62 7 21 7"
+              style={{ fill: "#2ca9bc", strokeWidth: 2 }}
+            ></polygon>
+            <path
+              id="primary-upstroke"
+              d="M11,20.5h.1m5.9,0h.1"
+              style={{
+                fill: "none",
+                stroke: "#000000",
+                strokeLinecap: "round",
+                strokeLinejoin: "round",
+                strokeWidth: 2.5,
+              }}
+            ></path>
+            <path
+              id="primary"
+              d="M3,3H5.14a1,1,0,0,1,1,.85L6.62,7,8,16l11-1,2-8H6.62"
+              style={{
+                fill: "none",
+                stroke: "#000000",
+                strokeLinecap: "round",
+                strokeLinejoin: "round",
+                strokeWidth: 2,
+              }}
+            ></path>
+          </g>
+        </svg>
+      </button>
+    </div>
+  );
+};
+
+export default Navbar;
